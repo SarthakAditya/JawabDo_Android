b360ee7bdc70b20488815d9e0df47ca1bbc79404
Sarthak Aditya <sarthak16188@iiitd.ac.in>
2019-11-07 11:32:25 +0530

    Fix Adding null/Existing courses and show correct Instructor name

---
+++ app/src/main/java/com/example/jawaabdo/AlertReceiver.java
@@ -0,0 +1,6 @@
+package com.example.jawaabdo;
+
+import android.content.BroadcastReceiver;
+
+public class AlertReceiver extends BroadcastReceiver {
+}
--- app/src/main/java/com/example/jawaabdo/Courses.java
+++ app/src/main/java/com/example/jawaabdo/Courses.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,6 +19,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,6 +31,7 @@ import org.json.JSONObject;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -39,21 +43,23 @@ public class Courses extends AppCompatActivity implements AdapterView.OnItemClic
     String user_id;
     ListView list;
     String[] courseslist ={} ;
+    String[] instructorList ={} ;
     Quizdata quizdata = new Quizdata();
     Button button ;
     TextView textView  ;
     FirebaseDatabase database1 = FirebaseDatabase.getInstance();
-    DatabaseReference myRef1 ;
+    DatabaseReference myRef1,CoursesRef,SingleCourseRef ;
     //    String[] instructorName={};
     int image=R.drawable.quiz;
     ArrayList<String> names = new ArrayList<>();;
+    ArrayList<String> Instnames = new ArrayList<>();;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
         button = (Button) findViewById(R.id.add_course) ;
         textView = (TextView) findViewById(R.id.course_id) ;
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
         user_id=getIntent().getStringExtra("userid");
         DatabaseReference myRef = database.getReference("Users/"+user_id +"/Courses");
 
@@ -65,43 +71,95 @@ public class Courses extends AppCompatActivity implements AdapterView.OnItemClic
 //                list.setAdapter(new MyAdapter(Courses.this,new String[0],image));
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
+                CoursesRef = database.getReference("Courses/");
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Log.d("my_message", snapshot.toString());
                     String name = snapshot.getValue(String.class);
                     names.add(name);
                 }
-                int numofcourses=names.size();
-                courseslist = new String[numofcourses];
+
+                CoursesRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot_C) {
+                        for (DataSnapshot snapshot_C : dataSnapshot_C.getChildren()) {
+                            for (int i=0 ; i < names.size() ; i++)
+                            {
+                                if (snapshot_C.getKey().equals(names.get(i)))
+                                {
+                                    /*if (snapshot_C.child("Time").exists())
+                                    {
+                                        Calendar c = Calendar.getInstance();
+                                        c.set(Calendar.MONTH,Integer.parseInt(snapshot_C.child("Time").child("Month").getValue().toString()));
+                                        c.set(Calendar.DATE,Integer.parseInt(snapshot_C.child("Time").child("Date").getValue().toString()));
+                                        c.set(Calendar.HOUR_OF_DAY,Integer.parseInt(snapshot_C.child("Time").child("HRS").getValue().toString()));
+                                        c.set(Calendar.MINUTE,Integer.parseInt(snapshot_C.child("Time").child("MINS").getValue().toString()));
+
+                                        Log.d("SarthakAditya","Notification To be Launched at" + c);
+
+                                        Intent intent = new Intent(Courses.this,AlertReceiver.class);
+                                        intent.putExtra("courseID",snapshot_C.getKey().toString());
+
+                                        PendingIntent alarmintent = PendingIntent.getBroadcast(Courses.this,0,intent,PendingIntent.FLAG_CANCEL_CURRENT);
+
+                                        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                                        long alarmstarttime = c.getTimeInMillis();
+                                        alarmManager.set(AlarmManager.RTC_WAKEUP,alarmstarttime,alarmintent);
+
+                                        Toast.makeText(getApplicationContext(), "Notification Set", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                    else
+                                        Log.d("SarthakAditya","Not Tests Found");*/
+                                    Instnames.add(snapshot_C.child("Instructor").getValue().toString());;
+                                }
+                            }
+                        }
+
+                        int numofcourses=names.size();
+                        courseslist = new String[numofcourses];
+                        instructorList = new String[numofcourses];
 //                instructorName=new String[numofcourses];
 
-                int i=0;
-                for(String name : names) {
-                    courseslist[i++]=name;
+                        int i=0;
+                        for(String name : names) {
+                            courseslist[i++]=name;
 
-                }
+                        }
+                        int j = 0;
+                        for (String iname : Instnames){
+                            instructorList[j++]=iname;
+                        }
 
 //                instructorName=courseslist;
-                list=(ListView)findViewById(R.id.coursesList);
-                if(courseslist.length == 0)
-                {
-                    (findViewById(R.id.textView3)).setVisibility(View.VISIBLE);
-                    list.setVisibility(View.INVISIBLE);
-                }
-                else{
-                    (findViewById(R.id.textView3)).setVisibility(View.INVISIBLE);
-                    list.setVisibility(View.VISIBLE);
+                        list=(ListView)findViewById(R.id.coursesList);
+                        if(courseslist.length == 0)
+                        {
+                            (findViewById(R.id.textView3)).setVisibility(View.VISIBLE);
+                            list.setVisibility(View.INVISIBLE);
+                        }
+                        else{
+                            (findViewById(R.id.textView3)).setVisibility(View.INVISIBLE);
+                            list.setVisibility(View.VISIBLE);
 
-                    MyAdapter adapter = new MyAdapter(Courses.this, courseslist, image);
-                    list.setAdapter(adapter);
-                    list.setOnItemClickListener(Courses.this);
-                    Log.d("my_message",courseslist[0]);
+                            MyAdapter adapter = new MyAdapter(Courses.this, courseslist, image , instructorList);
+                            list.setAdapter(adapter);
+                            list.setOnItemClickListener(Courses.this);
+                            Log.d("my_message",courseslist[0]);
 //                for(String name : names) {
 //                    TextView stringTextView = (TextView) findViewById(R.id.showCoursesTextView);
 //                    stringTextView.setText(stringTextView.getText().toString() + name + " ,");
 //
 //                }
 //                Log.d("Fire", "Value is: " + value);
-                }}
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
 
             @Override
             public void onCancelled(DatabaseError error) {
@@ -111,13 +169,55 @@ public class Courses extends AppCompatActivity implements AdapterView.OnItemClic
         });
     }
     public void add_listener(View view){
-        String cid = textView.getText().toString() ;
 
-        myRef1 = database1.getReference("Users/" + user_id + "/Courses") ;
-        myRef1.child(cid).setValue(cid) ;
-        Intent intent = getIntent();
-        finish();
-        startActivity(intent);
+        CoursesRef = database1.getReference("Courses");
+
+        CoursesRef.addValueEventListener(new ValueEventListener() {
+            String cid = textView.getText().toString() ;
+            boolean flag = false;
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String name = snapshot.getKey();
+                    if (name.equals(cid))
+                        flag=true;
+                }
+
+                if (flag==true)
+                {
+                    boolean flag2 = true;
+                    for (int i = 0; i<names.size(); i++)
+                    {
+                        if (cid.equals(names.get(i)))
+                        {
+                            flag2 = false;
+                        }
+                    }
+                    if (flag2)
+                    {
+                        myRef1 = database1.getReference("Users/" + user_id + "/Courses");
+                        myRef1.child(cid).setValue(cid);
+                        SingleCourseRef = database1.getReference("Courses/"+cid+"/Students");
+                        SingleCourseRef.child(user_id).setValue(user_id);
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "Course is already added", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "Course does not exist", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
     public void onItemClick(AdapterView<?>parent,View view,int position,long id){
@@ -132,12 +232,14 @@ public class Courses extends AppCompatActivity implements AdapterView.OnItemClic
     class MyAdapter extends ArrayAdapter<String>{
         Context context;
         String[] cnames;
+        String[] instnames;
         int image;
-        MyAdapter(Context c,String[] cnames,int image){
+        MyAdapter(Context c,String[] cnames,int image, String[] instnames){
             super(c,R.layout.row,R.id.cname,cnames);
             this.context=c;
             this.cnames=cnames;
             this.image=image;
+            this.instnames = instnames;
         }
         @Nonnull
 
@@ -148,11 +250,12 @@ public class Courses extends AppCompatActivity implements AdapterView.OnItemClic
             View row = layoutInflater.inflate(R.layout.row, parent, false);
             ImageView images = row.findViewById(R.id.image);
             TextView myTitle = row.findViewById(R.id.cname);
-//            TextView myDescription = row.findViewById(R.id.iname);
+            TextView myDescription = row.findViewById(R.id.iname);
 
             // now set our resources on views
             images.setImageResource(image);
             myTitle.setText(cnames[position]);
+            myDescription.setText(instnames[position]);
 
 
 
